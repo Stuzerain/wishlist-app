45a606e45208afe7dbaeff011a0f6c550c644b84
Stuzerain <shanford3@gmail.com>
2021-01-22 12:09:14 -0500

    setup of basic bundling

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './client/public'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      // {
+      //   test: /\.css$/,
+      //   loader: 'style-loader!css-loader?modules&importLoaders=1'
+      // },
+    ]
+  }
+}
