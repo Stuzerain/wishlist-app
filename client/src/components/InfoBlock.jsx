6918bafeda2cc80964aba5a54a4d8ca046d53e52
Stuzerain <shanford3@gmail.com>
2021-01-28 12:23:34 -0500

    server API can post people to db, handled edge case where no ideas caused error in InfoBlock comp

--- client/src/components/InfoBlock.jsx
+++ client/src/components/InfoBlock.jsx
@@ -10,18 +10,26 @@ const InfoBlock = () => {
 
   const { selected } = useSelector(s => s.home);
 
-  const ideas = selected.ideas.map((idea, index) => (
-    <Col key={index}>
+  const mapper = (input) => {
+    return input.ideas.map((idea, index) => {
+      <Col key={index}>
         {idea}
-    </Col>
-  ));
+      </Col>
+    })
+  }
+
+  // const ideas = selected.ideas.map((idea, index) => (
+  //   <Col key={index}>
+  //       {idea}
+  //   </Col>
+  // ));
 
   return (
     <Container style={{textAlign: 'center'}}>
       <Row>
         <Col>Name: {selected.name}</Col>
       </Row>
-      <Row>{ideas}</Row>
+      <Row> { selected.ideas ? mapper(selected) : 'no ideas yet' } </Row>
     </Container>
   )
 }
